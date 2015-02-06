9f0d2a5937579779782c0b58c1b0cfda641aa30a
swolfod <swolfod@gmail.com>
2015-02-06 20:49:14 +0800

    database engine switched to mysql

--- Lushu_Mini/settings.py
+++ Lushu_Mini/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'Lushu_Mini.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lushu_mini',
+        'USER': 'root',
+        'PASSWORD': '1q2w3e4r',
+        'HOST': '127.0.0.1',
+        'PORT': ''
     }
 }
 
