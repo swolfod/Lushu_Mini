53618f4f75f1c58b8aaf52b1e39e65a404f5dc1f
swolfod <swolfod@gmail.com>
2015-02-09 12:18:38 +0800

    refresh wechat token
    refresh wechat token
    
    refresh wechat token
    
    refresh wechat token

--- WechatApi/views.py
+++ WechatApi/views.py
@@ -1,15 +1,9 @@
 __author__ = 'swolfod'
 
-from .config import *
-from django.core.urlresolvers import reverse
 from django.http import *
 from django.shortcuts import HttpResponseRedirect
-import datetime
-import json
-from urllib.parse import quote_plus, unquote
-from .wechatUtils import wechatAuthUrl
-from utilities import utils, djangoUtils
-from .models import WechatAccount
+from urllib.parse import unquote
+from .wechatUtils import refreshWechatToken
 
 
 def wechatSignature(request):
@@ -25,50 +19,7 @@ def authCallback(request):
         return authFailed(request, state)
 
     try:
-        accessUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={0}&secret={1}&code={2}&grant_type=authorization_code".format(
-            AppId,
-            SecretKey,
-            code
-        )
-
-        link, content, session = utils.LoadHttpString(accessUrl, encoding="utf-8", timeout=10)
-        accessInfo = json.loads(content)
-
-        access_token = accessInfo["access_token"]
-        accessExpires = int(accessInfo["expires_in"])
-        openid = accessInfo["openid"]
-
-        request.session["accressToken"] = access_token
-        request.session["accessExpiry"] = (datetime.datetime.now() + datetime.timedelta(0, accessExpires)).strftime('%Y-%m-%d %H:%M:%S')
-        request.session["refreshToken"] = accessInfo["refresh_token"]
-        request.session["openid"] = openid
-        request.session["scope"] = accessInfo["scope"]
-
-        # Refresh token expires in 30 days
-        request.session.set_expiry(3600 * 24 * 30)
-
-        userInfoUrl = "https://api.weixin.qq.com/sns/userinfo?access_token={0}&openid={1}&lang=zh_CN".format(access_token, openid)
-        link, content, session = utils.LoadHttpString(userInfoUrl, session=session, encoding="utf-8", timeout=10)
-        userInfo = json.loads(content)
-
-        openid = userInfo["openid"]
-        wechatAccount = djangoUtils.getOrNone(WechatAccount, openid=openid)
-        if not wechatAccount:
-            wechatAccount = WechatAccount(openid=openid)
-
-        wechatAccount.nickname = userInfo["nickname"]
-        wechatAccount.unionid = userInfo.get("unionid", None)
-        wechatAccount.sex = int(userInfo.get("sex", 1)) == 1
-        wechatAccount.province = userInfo.get("province", None)
-        wechatAccount.city = userInfo.get("city", None)
-        wechatAccount.country =  userInfo.get("country", None)
-        wechatAccount.headimgurl = userInfo.get("headimgurl", None)
-        wechatAccount.privilege = userInfo.get("privilege", None)
-
-        wechatAccount.save()
-
-        request.session["userInfo"] = content
-
+        refreshWechatToken(request, code=code)
         redirectUrl = unquote(state) if state else "/"
         return HttpResponseRedirect(redirectUrl)
     except:
--- WechatApi/wechatUtils.py
+++ WechatApi/wechatUtils.py
@@ -4,11 +4,13 @@ from .config import *
 from django.core.urlresolvers import reverse
 from django.shortcuts import HttpResponseRedirect
 from urllib.parse import quote_plus
-from utilities import djangoUtils
+from utilities import djangoUtils, utils
 from .models import *
 from django.conf import settings
 from uuid import uuid1
 from random import randint
+import json
+from datetime import datetime, timedelta
 
 WECHAT_DEBUG = getattr(settings, "WECHAT_DEBUG", True)
 
@@ -48,6 +50,13 @@ def getAccountById(openid):
 
 def getCurrentAccount(request):
     openid = request.session.get("openid")
+    if not WECHAT_DEBUG:
+        accessToken = request.session.get("accessToken")
+        accessExpiry = request.session.get("accessExpiry")
+        if not accessToken or not accessExpiry or datetime.strptime(accessExpiry, '%Y-%m-%d %H:%M:%S') < datetime.now():
+            refreshWechatToken(request, refreshToken=request.session["refreshToken"])
+            openid = request.session["openid"]
+
     account = getAccountById(openid)
 
     if not account and WECHAT_DEBUG:
@@ -64,3 +73,54 @@ def getCurrentAccount(request):
         return account
 
     return account
+
+
+def refreshWechatToken(request, code=None, refreshToken=None):
+    if code:
+        accessUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={0}&secret={1}&code={2}&grant_type=authorization_code".format(
+            AppId,
+            SecretKey,
+            code
+        )
+    elif refreshToken:
+        accessUrl = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid={0}&grant_type=refresh_token&refresh_token={1}".format(AppId, refreshToken)
+    else:
+        raise Exception()
+
+    link, content, session = utils.LoadHttpString(accessUrl, encoding="utf-8", timeout=10)
+    accessInfo = json.loads(content)
+
+    access_token = accessInfo["access_token"]
+    accessExpires = int(accessInfo["expires_in"])
+    openid = accessInfo["openid"]
+
+    request.session["accessToken"] = access_token
+    request.session["accessExpiry"] = (datetime.now() + timedelta(0, accessExpires)).strftime('%Y-%m-%d %H:%M:%S')
+    request.session["refreshToken"] = accessInfo["refresh_token"]
+    request.session["openid"] = openid
+    request.session["scope"] = accessInfo["scope"]
+
+    # Refresh token expires in 30 days
+    request.session.set_expiry(3600 * 24 * 30)
+
+    userInfoUrl = "https://api.weixin.qq.com/sns/userinfo?access_token={0}&openid={1}&lang=zh_CN".format(access_token, openid)
+    link, content, session = utils.LoadHttpString(userInfoUrl, session=session, encoding="utf-8", timeout=10)
+    userInfo = json.loads(content)
+
+    openid = userInfo["openid"]
+    wechatAccount = djangoUtils.getOrNone(WechatAccount, openid=openid)
+    if not wechatAccount:
+        wechatAccount = WechatAccount(openid=openid)
+
+    wechatAccount.nickname = userInfo["nickname"]
+    wechatAccount.unionid = userInfo.get("unionid", None)
+    wechatAccount.sex = int(userInfo.get("sex", 1)) == 1
+    wechatAccount.province = userInfo.get("province", None)
+    wechatAccount.city = userInfo.get("city", None)
+    wechatAccount.country =  userInfo.get("country", None)
+    wechatAccount.headimgurl = userInfo.get("headimgurl", None)
+    wechatAccount.privilege = userInfo.get("privilege", None)
+
+    wechatAccount.save()
+
+    request.session["userInfo"] = content
